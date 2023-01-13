b97aa25ca53be914d9f7d63fad4442c95f6f5ccd
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-01-13 14:31:34 +0100

    ajout css, js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let playSound = () => new Audio("/assets/audio/bienvenue-a-boooord.mp3").play()
+
+let playSound2 = () => new Audio("/assets/audio/estceque.mp3").play()
+
+console.log(playSound, "ca cliiiique");
